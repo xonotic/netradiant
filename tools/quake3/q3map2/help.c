89a811052fa39b15c28e2ca257623e43e87425e7
Thomas Debesse <dev@illwieckz.net>
2024-10-25 22:18:02 +0200

    q3map2: do not use the new area light backsplash algorithm by Jelvan by default, make it optional
    NRC commit ed4c8c204443726e852a4c8927b3f8d2571cc522 replaced
    the historic Quake 3 backsplash algorithm with an alternate
    one by Jelvan:
    
    > new area lights backsplash algorithm ( by Jelvan ),
    > hijacking temp area lights ( to simulate volumetric
    > behavior of source ones )
    
    It suffers from many bugs and then only a few maps may benefit from it,
    see: https://gitlab.com/xonotic/netradiant/-/merge_requests/210

--- tools/quake3/q3map2/help.c
+++ tools/quake3/q3map2/help.c
@@ -181,6 +181,8 @@ void HelpLight()
 		{"-vlight [options] <filename.map>", "Deprecated alias for `-light -fast` ... filename.map"},
 		{"-approx <N>", "Vertex light approximation tolerance (never use in conjunction with deluxemapping)"},
 		{"-areascale <F, `-area` F>", "Scaling factor for area lights (surfacelight)"},
+		{"-backsplashmode 0", "Use point light as backsplash light"},
+		{"-backsplashmode 1", "Use area light as backsplash light (experimental)"},
 		{"-border", "Add a red border to lightmaps for debugging"},
 		{"-bouncegrid", "Also compute radiosity on the light grid"},
 		{"-bounceonly", "Only compute radiosity"},
